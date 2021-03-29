b2d28d669e8effc39e47df26ccb2f9b75edc7d17
acy238 <78458967+acy238@users.noreply.github.com>
2021-03-28 18:09:29 -0700

    Update tests.py

--- GiftcardSite/LegacySite/tests.py
+++ GiftcardSite/LegacySite/tests.py
@@ -37,7 +37,7 @@ class TestResponses(TestCase):
         res = self.client.post('/gift/1', {"director": "%3Cscript%3Evar%20xhr%20=%20new%20XMLHttpRequest();%20xhr.open(%22POST%22,%20%22/gift/1%22,%20true);var%20data%20=%20new%20FormData();%20data.append(%27username%27,%27john%27);%20data.append(%27amount%27,333);%20data.append(%27product%27,%20%271%27);%20xhr.send(data);%3C/script%3E"})
         self.assertEqual(res.status_code, 302)
         
-    #Testing for SQL injection in uploaded giftcard to retrieve admin user's salted password
+    #Testing for SQL injection in uploaded giftcard to retrieve admin user's salted password, related to both attack number 3 and number 4
     def test_a3(self):
         self.client.post("/login/", {"uname":"john", "pword":"john"})
         data = {}
